afaa0d3709de316d2be28b71d41e52740b0ca31d
Elliot Jones <ejones@1D4.42angouleme.fr>
2026-03-19 17:43:22 +0100

    I have a philosophers that works without number of meals, timestamp is a bit off, I think.

--- src/main.c
+++ src/main.c
@@ -4,51 +4,59 @@ void	*philo_routine(void *arg)
 {
 	t_philo		*philo;
 	struct timeval	st_death_evt;
+	struct timeval	start;
 	short int	left;
 	short int	right;
 
 	philo = (t_philo *)arg;
 	gettimeofday(&st_death_evt, NULL);
+	gettimeofday(&start, NULL);
 	left = philo->id;
 	right = (philo->id + 1) % philo->nb_philos;
-	while (philo->state != DEAD 
-	&& philo->time_to_die - elapsed_time(st_death_evt) >= 0)
+	while (philo->time_to_die - elapsed_time(st_death_evt) >= 0)
 	{
-		pthread_mutex_lock(&philo->forks[left]);
-		pthread_mutex_lock(&philo->forks[right]);
+		ft_lock(philo, left, right);
+		printf("%ld %d has taken a fork\n", elapsed_time(start), philo->id);
 		philo->state = EATING;
-		if (!ft_event(&st_death_evt, philo))
-			break;
-		philo->state = SLEEPING;		
-		pthread_mutex_lock(&philo->forks[left]);
-		pthread_mutex_unlock(&philo->forks[right]);
-		if (!ft_event(&st_death_evt, philo))
-			break;
+		gettimeofday(&st_death_evt, NULL);
+		printf("%ld %d is eating\n", elapsed_time(start), philo->id);
+		usleep(philo->time_to_eat * 1000);
+
+		ft_unlock(philo, left, right);
+
+		philo->state = SLEEPING;
+		printf("%ld %d is sleeping\n", elapsed_time(start), philo->id);
+		usleep(philo->time_to_sleep * 1000);
+
 		philo->state = THINKING;
+		printf("%ld %d is thinking\n", elapsed_time(start), philo->id);
+
 	}
+	printf("philo %d is DEAD\n", philo->id);
 	return (NULL);
 }
 
 t_philo	*init_philos(char **av)
 {
-	int		i;
+	short int	i;
 	short int	nb_philos;
 	t_philo		*philos;
 	pthread_mutex_t	*forks;
-	
+
 	i = 0;
-	nb_philos = atoi(av[1]);	
+	nb_philos = ft_atoi(av[1]);
 	philos = malloc(sizeof(t_philo) * nb_philos);
 	forks = malloc(sizeof(pthread_mutex_t) * nb_philos);
 	while (i < nb_philos)
 	{
 		philos[i].id = i;
 		philos[i].nb_philos = nb_philos;
+		philos[i].end_simulation = philos[0].end_simulation;
 		pthread_mutex_init(&forks[i], NULL);
 		philos[i].forks = forks;
-		philos[i].time_to_die = atoi(av[2]);
-		philos[i].time_to_eat = atoi(av[3]);
-		philos[i].time_to_sleep = atoi(av[4]);
+		philos[i].time_to_die = ft_atoi(av[2]);
+		philos[i].time_to_eat = ft_atoi(av[3]);
+		philos[i].time_to_sleep = ft_atoi(av[4]);
 		philos[i].state = THINKING;
 		i++;
 	}
@@ -58,13 +66,14 @@ t_philo	*init_philos(char **av)
 int	main(int ac, char **av)
 {
 	t_philo		*philos;
-	
+
 	philos = NULL;
 	if (!parsing(ac, av))
-		return (0);	
+		return (0);
 	philos = init_philos(av);
 	manage_thread(philos, philos[0].forks, philos[0].nb_philos);
 	free(philos[0].forks);
+	free(philos[0].end_simulation);
 	free(philos);
 	return (0);
 }
